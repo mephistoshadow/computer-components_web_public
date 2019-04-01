4d06c7d8208944b16d30a5660331281a3a2bc284
mephistoshadow <siqi.yang@mail.utoronto.ca>
2019-04-01 15:12:21 -0400

    relocate again

--- phase2/backEnd/models/product.js
+++ phase2/backEnd/models/product.js
@@ -1,40 +1,40 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
 
-const validator = require('validator')
-const bcrypt = require('bcryptjs')
-
-
-
-
-// note, the review schema to be saved in the database
-// should include the 
-// time stamp, 
-// comment content
-// product infomation 
-
-
-const ReviewSchema = new mongoose.Schema({
-	title: String
-	date: String,
-	name: Number,
-	content: String,
-	userId: String 			// correct type? 
-	}
-);
-
-const ProductSchema = new mongoose.Schema({    
-	name: String,
-	img_url: String, 
-	title_description: String,
-	description: String, 
-	reviews: [ReviewSchema]
-});
+// const validator = require('validator')
+// const bcrypt = require('bcryptjs')
 
 
 
 
+// // note, the review schema to be saved in the database
+// // should include the 
+// // time stamp, 
+// // comment content
+// // product infomation 
 
-const Product = mongoose.model('Product', ProductSchema);
 
-module.exports = { Product };
+// const ReviewSchema = new mongoose.Schema({
+// 	title: String
+// 	date: String,
+// 	name: Number,
+// 	content: String,
+// 	userId: String 			// correct type? 
+// 	}
+// );
+
+// const ProductSchema = new mongoose.Schema({    
+// 	name: String,
+// 	img_url: String, 
+// 	title_description: String,
+// 	description: String, 
+// 	reviews: [ReviewSchema]
+// });
+
+
+
+
+
+// const Product = mongoose.model('Product', ProductSchema);
+
+// module.exports = { Product };
