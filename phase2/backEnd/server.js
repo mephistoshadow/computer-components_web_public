30ff31f73d9b2af41cad55c08617ed607fcf1fdb
lixun94 <root@DESKTOP-O03G45E.localdomain>
2019-04-01 14:52:36 -0400

    backedn

--- phase2/backEnd/server.js
+++ phase2/backEnd/server.js
@@ -11,17 +11,42 @@ Reservation: { time - String, people - Number }
 const log = console.log;
 
 const express = require('express');
+const session = require('express-session')
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
+const hbs = require('hbs')
 
 // Mongoose
 const { mongoose } = require('./db/mongoose');
 const { Product } = require('./models/product');
+const { User } = require('./models/user')
 
 // Express
 const port = process.env.PORT || 3000
 const app = express();
 app.use(bodyParser.json());
+
+
+
+// Middleware for authentication for resources
+const authenticate = (req, res, next) => {
+	if (req.session.user) {
+		User.findById(req.session.user).then((user) => {
+			if (!user) {
+				return Promise.reject()
+			} else {
+				req.user = user
+				next()
+			}
+		}).catch((error) => {
+			res.redirect('/login')
+		})
+	} else {
+		res.redirect('/login')
+	}
+}
+
+
 
 // Route for getting the main page
 app.get('/main', (req, res) =>{
@@ -32,13 +57,13 @@ app.get('/main', (req, res) =>{
 
 
 // route for login
-app.route('/login')
+app.route('/main/login')
 	.get(sessionChecker, (req, res) => {
 		res.sendFile(__dirname + '/public/login.html')
 	})
 
 //
-app.get('/profile', (req, res) => {
+app.get('/main/user', (req, res) => {
 	// check if we have active session cookie
 	if (req.session.user) {
 		//res.sendFile(__dirname + '/public/dashboard.html')
@@ -50,6 +75,74 @@ app.get('/profile', (req, res) => {
 		res.redirect('/login')
 	}
 })
+
+app.get('/main/admin', (req, res) => {
+	// check if we have active session cookie
+	if (req.session.user) {
+		//res.sendFile(__dirname + '/public/dashboard.html')
+		res.render('admin.hbs', {
+			email: req.session.email
+		})
+	else if()
+	} else {
+		res.redirect('/main/login')
+	}
+})
+
+
+// User login and logout routes
+
+app.post('/users/login', (req, res) => {
+	const email = req.body.email
+	const password = req.body.password
+
+	User.findByEmailPassword(email, password).then((user) => {
+		if(!user) {
+			res.redirect('/login')
+		}
+		
+		else {
+			// Add the user to the session cookie that we will
+			// send to the client
+			req.session.user = user._id;
+			req.session.email = user.email
+			req.session.role = user.role
+			if(user.role == "user"){
+				res.redirect('/main/user')
+			}
+			else{
+				res.redirect('/main/admin')
+			}
+		}
+	}).catch((error) => {
+		res.status(400).redirect('main/login')
+	})
+})
+
+//user logout
+
+app.get('/main/users/logout', (req, res) => {
+	req.session.destroy((error) => {
+		if (error) {
+			res.status(500).send(error)
+		} else {
+			res.redirect('/')
+		}
+	})
+})
+
+// admin logout
+
+app.get('/main/admin/logout', (req, res) => {
+	req.session.destroy((error) => {
+		if (error) {
+			res.status(500).send(error)
+		} else {
+			res.redirect('/')
+		}
+	})
+})
+
 
 
 
