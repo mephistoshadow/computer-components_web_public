7dd002a9dd5f00ba8164fcccdabddcdaa3478b7c
lixun94 <root@DESKTOP-O03G45E.localdomain>
2019-04-04 22:29:18 -0400

    add review

--- phase2/backEnd/server.js
+++ phase2/backEnd/server.js
@@ -455,7 +455,7 @@ app.post('/product/wish_list/:name', (req, res) => {
 					res.status(404).send()
 				} else {
 					log("sucessful")
-					log(p)
+					log(user)
 					res.send({user})
 				}
 			}).catch((error) => {
@@ -495,7 +495,6 @@ app.post('/product/review/:p_name', (req, res) => {
 		username: req.session.email
 		};
 	
-	log(review)
 	
 
 	Product.find({name: name}).then((product) => {
@@ -505,11 +504,12 @@ app.post('/product/review/:p_name', (req, res) => {
 			res.status(404).send()
 		} 
 		else {
+			log(review)
 			User.findByIdAndUpdate(req.session.user, {$push: {comment_history: review}}, {new: true}).then((user) => {
 				if (!user) {
 					res.status(404).send()
 				} else {
-					
+					log(user)
 					Product.findByIdAndUpdate(p._id,  {$push: {reviews: review}}, {new: true}).then((product) => {
 				if (!product) {
 					res.status(404).send()
